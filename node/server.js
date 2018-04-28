f218641665b867791eda6ea93c40078f3ecff492
ankmks <anukool.mks@gmail.com>
2018-04-28 09:50:02 +0700

    server.js  update

--- node/server.js
+++ node/server.js
@@ -1,1 +1,12 @@
 //new server.js for express-app
+
+const express = require('express'),
+                app = express();
+
+
+                app.use('/', (request, response) => {
+                    response.send('index page.');
+                });
+
+
+                app.listen(3000, console.log('Express  server started at port 3000'));
