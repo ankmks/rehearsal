f67b44627dc7dc102a0a84187f987bf3ba8533f4
ankmks <anukool.mks@gmail.com>
2018-05-06 21:59:59 +0700

    4-update

--- node/4-validate/routes/index.js
+++ node/4-validate/routes/index.js
@@ -3,17 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'cool', condition: false, myArray: [1,2,3] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
+  req.session.success = null;
 });
 
-router.get('/test/:id', function (req,res,next){
-  res.render('test',{outputto: req.params.id});
+router.post('/submit', function(req,res,next){
+  //
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min : 4}).equals(req.body.confirmpassword);
+
+  var errors = req.validationErrors();
+  if(errors){
+    req.session.errors  = errors;
+    req.session.succuss = false;
+  }else{
+    req.session.success = true;
+  }
+    res.redirect('/');
+
 });
 
-router.post('/test/submit', function(req,res,next){
-  var id = req.body.id;
-  
-  res.redirect('/test/'+id);
-});
 
 module.exports = router;
